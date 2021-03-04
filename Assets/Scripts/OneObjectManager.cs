a1877ab1fee7ddc4ad0334850955b71fb1661420
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2021-03-04 14:15:31 +0200

    Added HighPoly Earth model, Object rotation and Object re-placement

---
+++ Assets/Scripts/OneObjectManager.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OneObjectManager : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    private GameObject _spawnedObject;
+    private PlacementIndicator _placementIndicator;
+
+    void Start()
+    {
+        _placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                if (_spawnedObject == null)
+                {
+                    _spawnedObject = Instantiate(objectToSpawn,
+                        _placementIndicator.transform.position, _placementIndicator.transform.rotation);
+                    _spawnedObject.name = "Earth_Object";
+                }
+                else
+                {
+                    _spawnedObject.transform.position = _placementIndicator.transform.position;
+                    _spawnedObject.transform.rotation = _placementIndicator.transform.rotation;
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/RotationManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    private GameObject _objectToRotate;
+
+    public void RotateToRight()
+    {
+        if (_objectToRotate == null)
+        {
+            _objectToRotate = GameObject.Find("Earth_Object");
+        }
+
+        _objectToRotate.transform.Rotate(new Vector3(_objectToRotate.transform.rotation.x, _objectToRotate.transform.rotation.y - 5));
+    }
+
+    public void RotateToLeft()
+    {
+        if (_objectToRotate == null)
+        {
+            _objectToRotate = GameObject.Find("Earth_Object");
+        }
+
+        _objectToRotate.transform.Rotate(new Vector3(_objectToRotate.transform.rotation.x, _objectToRotate.transform.rotation.y + 5));
+    }
+}
